f9cea2aa98632d4b84bfc0330e6621659ecf4fd2
Geralyn <geralynnn@gmail.com>
2021-02-21 21:14:44 +0800

    Update DatingProfiles.kt
    - remove unused code
    - send resource id to retrieve image
    - add method to save user rating

--- GeralynMeetsBagel/app/src/main/java/com/myapplication/DatingProfiles.kt
+++ GeralynMeetsBagel/app/src/main/java/com/myapplication/DatingProfiles.kt
@@ -1,21 +1,17 @@
 package com.myapplication
 
+import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.drawToBitmap
 import com.myapplication.databinding.ActivityDatingProfilesBinding
-import com.myapplication.databinding.ActivityProfileBinding
+import java.io.PrintStream
 
 class DatingProfiles : AppCompatActivity() {
     private lateinit var binding: ActivityDatingProfilesBinding
     private lateinit var dateUser: String
-    private lateinit var dateDrawable: Drawable
     private lateinit var dateDetails: String
     private val REQ_CODE = 3
 
@@ -26,38 +22,59 @@ class DatingProfiles : AppCompatActivity() {
     }
 
     fun getProfileDetails(view: View) {
+        var dateResource = 0
         when (view.id) {
             R.id.card1 -> {
                 dateUser = binding.date1TextView.text.toString()
-                dateDrawable = binding.date1ImageView.drawable
                 dateDetails = "A very ambitious woman. Loves to work 9am-5pm."
+                dateResource = R.drawable.date1
             }
             R.id.card2 -> {
                 dateUser = binding.date2TextView.text.toString()
-                dateDrawable = binding.date2ImageView.drawable
                 dateDetails = "Likes to smile alot. Favourite color is blue."
+                dateResource = R.drawable.date2
             }
             R.id.card3 -> {
                 dateUser = binding.date3TextView.text.toString()
-                dateDrawable = binding.date3ImageView.drawable
                 dateDetails = "Clothing Model. Loves Instagram."
+                dateResource = R.drawable.date3
             }
             R.id.card4 -> {
                 dateUser = binding.date4TextView.text.toString()
-                dateDrawable = binding.date4ImageView.drawable
                 dateDetails = "Geek, likes to program."
+                dateResource = R.drawable.date4
             }
         }
 
-//        Toast.makeText(this, "$dateDrawable", Toast.LENGTH_SHORT).show()
-
         val detailsIt = Intent(this, ProfileDetails::class.java)
-        // Need to figure out how to pass drawable into intent.
         detailsIt.putExtra("dateUser", dateUser)
         detailsIt.putExtra("dateDetails", dateDetails)
+        detailsIt.putExtra("dateResource", dateResource)
         startActivityForResult(detailsIt, REQ_CODE)
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_CODE && data!=null) {
+            val user = data.getStringExtra("dateUser")
+            val filename = data.getStringExtra("filename")
+            val rating = data.getFloatExtra("userRating", 0F)
+
+            if (filename != null && user != null && rating != 0F) {
+                saveUserDetails(filename, user, rating)
+            }
+        }
+    }
+
+    private fun saveUserDetails(fname: String, user: String, rating: Float) {
+        val output = PrintStream(openFileOutput(fname, Context.MODE_PRIVATE))
+        output.println(user + "\t" + rating)
+        output.close()
+        Toast.makeText(this, "Your rating for $user: $rating", Toast.LENGTH_SHORT).show()
+    }
+
 
 
 }
